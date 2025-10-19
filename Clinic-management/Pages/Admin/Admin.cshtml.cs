2e05d315609f7070096b6dcbbad9a5ef40d08338
TrQHuy-build <trqhuy.it.build@gmail.com>
2025-10-19 20:43:26 +0700

    Initial commit - ASP.NET Clinic Management project

---
+++ Clinic-management/Pages/Admin/Admin.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clinic_management.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
---
+++ Clinic-management/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clinic_management.Pages
+{
+    public class NewPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
